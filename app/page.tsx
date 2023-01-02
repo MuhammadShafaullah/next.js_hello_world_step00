d583d70aa611da73097bf0501649614e6a211898
Shafaullah Malik <72027876+MuhammadShafaullah@users.noreply.github.com>
2023-01-02 20:24:35 +0500

    next.js
    its 2nd comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 
 export default function Home() {
   return (
-          <div>Hello World from Malik Shafaullah</div>
+          <div>
+            <h1>Welcome </h1>
+            <h1>Developers</h1><br />
+
+            <div>
+              <h4>Created By: Malik Shafaullah</h4>
+            </div>
+          </div>
+          
   )
 }
 
